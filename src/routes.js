bdfc58160a4a996aeafd84f25fef91e7cf6e0cf7
Nick Field <heyimnick@live.com>
2016-12-06 12:42:35 +0000

    add route and server

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,14 @@
+const username
 
+module.exports = {
+  method: 'GET',
+  path: '/authenticated-route',
+  config: {
+    auth: 'session',
+    handler(req, reply) {
+      if (req.auth.isAuthenticated) {
+        reply('You are allowed to see this');
+      }
+    },
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
+const Hapi = require('hapi');
+const CookieAuth = require('hapi-auth-cookie');
 
+const routes = require('./routes');
+
+const cookieOptions = {
+  password: 'thisisafakepasswordthisisafakepasswordthisisafakepassword',
+  cookie: 'app-cookie',
+  isSecure: false,
+  ttl: 24 * 60 * 60 * 1000,
+};
+
+const server = new Hapi.Server();
+
+server.connection({ port: 8080 });
+
+server.register(CookieAuth, (registerError) => {
+  if (registerError) throw registerError;
+
+  server.auth.strategy('session', 'cookie', cookieOptions);
+
+  server.route(routes);
+});
+
+module.exports = server;
